7adcc56f38860cfddad05a08aab63079f98b1d9b
Ravi <ravipkakadia1@gmail.com>
2026-02-25 21:37:12 -0800

    intial sidebar setup

--- src/components/SidebarListItem.jsx
+++ src/components/SidebarListItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../assets/styles/components.css'
 
 export default function SidebarListItem({ itemName, itemLink }) {
   return (
     <div className='sidebar-list-item'>
-        <Link to={itemLink}>{itemName}</Link>
+        <Link className='sidebar-list-item-link' to={itemLink}>{itemName}</Link>
     </div>
   )
 }
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import SidebarListItem from '../components/SidebarListItem'
+import '../assets/styles/sidebar.css'
 
 export default function Sidebar() {
   return (
     <div className='sidebar'>
-      <div className='profile-image'></div>
+      <div className='profile-image'>
+        
+      </div>
       <div className='contact'></div>
       <div className='sidebar-list'>
         {/* Home -> summary, skill, reviews */}
