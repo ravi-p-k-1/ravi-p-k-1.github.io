e0d91bf91be5861f3d569f9a60e9a611b9515233
Ravi <ravipkakadia1@gmail.com>
2026-02-27 16:30:36 -0800

    Hover Indicator for sidebar implemented with Inter as font family from google font

--- src/components/SidebarListItem.jsx
+++ src/components/SidebarListItem.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../assets/styles/components.css'
 
-export default function SidebarListItem({ itemName, itemLink }) {
+export default function SidebarListItem({ itemName, itemLink, hoverFunction }) {
   return (
-    <div className='sidebar-list-item'>
+    <div className='sidebar-list-item' onMouseEnter={hoverFunction}>
         <Link className='sidebar-list-item-link' to={itemLink}>{itemName}</Link>
     </div>
   )
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import { useRef, useState } from 'react'
 import SidebarListItem from '../components/SidebarListItem'
 import '../assets/styles/sidebar.css'
 
 export default function Sidebar() {
+
+  // Home -> summary, skill, reviews
+  // About -> Education, Certifications, Profiles
+  const navItems = [
+    { name: "Home", link: "/" },
+    { name: "Work Experience", link: "/work-experience" },
+    { name: "Projects", link: "/projects" },
+    { name: "About", link: "/about" }
+  ];
+
+  const navRef = useRef(null);
+  const [indicatorStyle, setIndicatorStyle] = useState({
+    opacity: 0,
+    transform: "translateY(0px)"
+  });
+
+  const moveHoverIndicator = (e) => {
+    const item = e.currentTarget;
+    const navTop = navRef.current.getBoundingClientRect().top;
+    const itemTop = item.getBoundingClientRect().top;
+    const y = itemTop - navTop;
+
+    setIndicatorStyle({
+      opacity: 1,
+      transform: `translateY(${y}px)`
+    });
+  }
+
+  const hideHoverIndicator = () => setIndicatorStyle((prev) => ({...prev, opacity: 0}));
+
   return (
     <div className='sidebar'>
       <div className='profile-image'>
         
       </div>
       <div className='contact'></div>
-      <div className='sidebar-list'>
-        {/* Home -> summary, skill, reviews */}
-        <SidebarListItem itemName={"Home"} itemLink={"/"}/>
-        <SidebarListItem itemName={"Work Experience"} itemLink={"/work-experience"}/>
-        <SidebarListItem itemName={"Projects"} itemLink={"/projects"}/>
-        {/* About -> Education, Certifications, Profiles */}
-        <SidebarListItem itemName={"About"} itemLink={"/about"}/>
+      <div className='sidebar-list' ref={navRef} onMouseLeave={hideHoverIndicator}>
+        <span className="hover-indicator" style={indicatorStyle}></span>
+        {
+          navItems.map((item, index) => (
+            <SidebarListItem key={index} itemName={item.name} itemLink={item.link} hoverFunction={moveHoverIndicator} />
+          ))
+        }
       </div>
     </div>
   )
