f0aee28afdfd64a32e548853d8100604ffbedf14
Ravi <ravipkakadia1@gmail.com>
2026-05-19 16:30:07 -0700

    Fix separator diamond color on mobile

--- src/components/Separator.jsx
+++ src/components/Separator.jsx
@@ -4,7 +4,7 @@ export default function Separator() {
   return (
     <div className='separator'>
         <div className='line'></div>
-        <span className='diamond'>♦</span>
+        <span className='diamond' aria-hidden='true'></span>
         <div className='line'></div>
     </div>
   )
